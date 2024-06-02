127e2314c6373ca56f4a81de3bdc1d968cdedb95
Veerawatt020253 <53421@yupparaj.ac.th>
2024-06-02 15:21:53 +0700

    edit title learntogether

--- LearnTogether/index.php
+++ LearnTogether/index.php
@@ -11,7 +11,7 @@ require_once("../config/conn.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>YRC Student Council</title>
+    <title>YRC Student Council - LearnTogether</title>
 
     <!-- icon -->
     <link rel="shortcut icon" href="../img/logo.png" type="image/x-icon">
