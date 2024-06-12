5f6048bd8631cb0a777be908ab537fb682dd6d3c
veerawatt020253 <53421@yupparaj.ac.th>
2024-06-12 18:52:56 +0700

    sql and post1

--- component/Slider.php
+++ component/Slider.php
@@ -5,17 +5,14 @@
             <button id="prev-slide" class="slide-button material-symbols-rounded">
                 <i class="fa-solid fa-arrow-left"></i>
             </button>
+            <?php
+            $sql = "SELECT * FROM announce";
+            $stmt = $pdo->query($sql);
+            ?>
             <ul class="image-list">
-                <img class="image-item" src="images/img-1.jpg" alt="img-1" />
-                <img class="image-item" src="images/img-2.jpg" alt="img-2" />
-                <img class="image-item" src="images/img-3.jpg" alt="img-3" />
-                <img class="image-item" src="images/img-4.jpg" alt="img-4" />
-                <img class="image-item" src="images/img-5.jpg" alt="img-5" />
-                <img class="image-item" src="images/img-6.jpg" alt="img-6" />
-                <img class="image-item" src="images/img-7.jpg" alt="img-7" />
-                <img class="image-item" src="images/img-8.jpg" alt="img-8" />
-                <img class="image-item" src="images/img-9.jpg" alt="img-9" />
-                <img class="image-item" src="images/img-10.jpg" alt="img-10" />
+                <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                    <img class="image-item" src="./img/post/<?php echo $row['img'] ?>" alt="img-<?php echo $row['id'] ?>" />
+                <?php } ?>
             </ul>
             <button id="next-slide" class="slide-button material-symbols-rounded">
                 <i class="fa-solid fa-arrow-right"></i>
---
+++ student-council.sql
@@ -0,0 +1,169 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2024 at 01:50 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student-council`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'Nice2250', 'Nice2250');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announce`
+--
+
+CREATE TABLE `announce` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `decp` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner3`
+--
+
+CREATE TABLE `banner3` (
+  `id` int(11) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `title`, `content`) VALUES
+(1, 'banner', 'Banner1.png'),
+(2, 'banner2', 'Banner2.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `viewer`
+--
+
+CREATE TABLE `viewer` (
+  `id` int(11) NOT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `announce`
+--
+ALTER TABLE `announce`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `banner3`
+--
+ALTER TABLE `banner3`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `viewer`
+--
+ALTER TABLE `viewer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `announce`
+--
+ALTER TABLE `announce`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `banner3`
+--
+ALTER TABLE `banner3`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `setting`
+--
+ALTER TABLE `setting`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `viewer`
+--
+ALTER TABLE `viewer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
