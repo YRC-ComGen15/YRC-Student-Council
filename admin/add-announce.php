5be6da06606d869c7212d80a90f6112fa71d4245
veerawatt020253 <53421@yupparaj.ac.th>
2024-06-13 17:28:42 +0700

    add-announce1

--- admin/add-announce.php
+++ admin/add-announce.php
@@ -8,6 +8,87 @@ if (!isset($_SESSION['username'])) {
 }
 
 require_once "../config/conn.php";
+
+
+if (isset($_POST["title"])) {
+    //สร้างตัวแปรวันที่เพื่อเอาไปตั้งชื่อไฟล์ใหม่
+    $date1 = date("Ymd_His");
+    //สร้างตัวแปรสุ่มตัวเลขเพื่อเอาไปตั้งชื่อไฟล์ที่อัพโหลดไม่ให้ชื่อไฟล์ซ้ำกัน
+    $numrand = (mt_rand());
+    $img_file = (isset($_POST['img_file']) ? $_POST['img_file'] : '');
+    $upload = $_FILES['img_file']['name'];
+
+    //มีการอัพโหลดไฟล์
+    if ($upload != '') {
+        //ตัดขื่อเอาเฉพาะนามสกุล
+        $typefile = strrchr($_FILES['img_file']['name'], ".");
+
+        //สร้างเงื่อนไขตรวจสอบนามสกุลของไฟล์ที่อัพโหลดเข้ามา
+        if ($typefile == '.jpg' || $typefile  == '.jpeg' || $typefile  == '.png') {
+
+            //โฟลเดอร์ที่เก็บไฟล์
+            $path = "upload/";
+            //ตั้งชื่อไฟล์ใหม่เป็น สุ่มตัวเลข+วันที่
+            $newname = $numrand . $date1 . $typefile;
+            $path_copy = $path . $newname;
+            //คัดลอกไฟล์ไปยังโฟลเดอร์
+            move_uploaded_file($_FILES['img_file']['tmp_name'], $path_copy);
+
+            //ประกาศตัวแปรรับค่าจากฟอร์ม
+            $title = $_POST['title'];
+            $decp = $_POST['decp'];
+            $date = $_POST['date'];
+
+            //sql insert
+            $stmt = $conn->prepare("INSERT INTO announce (title decp, img, date)
+    VALUES (:title, :decp, '$newname', :date)");
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindParam(':decp', $decp, PDO::PARAM_STR);
+            $stmt->bindParam(':date', $date, PDO::PARAM_STR);
+            $result = $stmt->execute();
+            //เงื่อนไขตรวจสอบการเพิ่มข้อมูล
+            if ($result) {
+                echo '<script>
+                     setTimeout(function() {
+                      swal({
+                          title: "อัพโหลดภาพสำเร็จ",
+                          type: "success"
+                      }, function() {
+                          window.location = "upload.php"; //หน้าที่ต้องการให้กระโดดไป
+                      });
+                    }, 1000);
+                </script>';
+            } else {
+                echo '<script>
+                     setTimeout(function() {
+                      swal({
+                          title: "เกิดข้อผิดพลาด",
+                          type: "error"
+                      }, function() {
+                          window.location = "upload.php"; //หน้าที่ต้องการให้กระโดดไป
+                      });
+                    }, 1000);
+                </script>';
+            } //else ของ if result
+
+
+        } else { //ถ้าไฟล์ที่อัพโหลดไม่ตรงตามที่กำหนด
+            echo '<script>
+                         setTimeout(function() {
+                          swal({
+                              title: "คุณอัพโหลดไฟล์ไม่ถูกต้อง",
+                              type: "error"
+                          }, function() {
+                              window.location = "upload.php"; //หน้าที่ต้องการให้กระโดดไป
+                          });
+                        }, 1000);
+                    </script>';
+        } //else ของเช็คนามสกุลไฟล์
+
+    } // if($upload !='') {
+
+    $conn = null; //close connect db
+} //isset
 ?>
 
 <!DOCTYPE html>
@@ -43,8 +124,20 @@ require_once "../config/conn.php";
             <h1><b><i class="fa-solid fa-bullhorn"></i> เพิ่มข่าวประชาสัมพันธ์</b></h1>
 
 
-            <form action="" method="post" class="border">
-                <input type="text" class="form-control w-50 mt-3">
+            <form action="" method="post" enctype="multipart/form-data">
+                <h3 class="mt-3">หัวข้อข่าวประชาสัมพันธ์</h3>
+                <input type="text" class="form-control w-100" placeholder="หัวข้อข่าว" name="title">
+
+                <h3 class="mt-3">รายละเอียดข่าวประชาสัมพันธ์</h3>
+                <textarea name="" class="form-control w-100" placeholder="รายละเอียดข่าวประชาสัมพันธ์" id=""></textarea>ฃ
+
+                <h3 class="mt-3">รูปข่าวประชาสัมพันธ์</h3>
+                <input type="file" class="form-control w-100" placeholder="หัวข้อข่าว" name="img_file">
+
+                <h3 class="mt-3">วันที่โพสต์</h3>
+                <input type="date" class="form-control w-100" placeholder="หัวข้อข่าว" name="date">
+
+                <input type="submit" class="btn btn-success mt-3 w-100" value="ตกลง">
             </form>
         </div>
     </main>
