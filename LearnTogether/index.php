189c9bfb3c4ce6ff4e8796be732021fa5a2cd96f
veerawatt020253 <woraprathuanhong@gmail.com>
2024-06-17 08:26:32 +0700

    learn2

---
+++ LearnTogether/index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learn Together</title>
+
+    <link rel="stylesheet" href="../Framework/bootstrap/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="./main.css">
+</head>
+<body>
+    
+<div class="d-flex mt-5 mt-6">
+    <a href="" class="m-auto mt-5 button">SHARE หนังสือ</a>
+
+</div>
+
+<div class="d-flex">
+    <a href="" class="m-auto mt-5 button">SHARE กิจกรรม</a>
+
+</div>
+
+<script src="../Framework/bootstrap/js/bootstrap.bundle.js"></script>
+</body>
+</html>
